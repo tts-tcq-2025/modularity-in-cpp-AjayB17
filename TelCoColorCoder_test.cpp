1d2f5d32458bb912728c9bdabcd293535d13abff
AjayB17 <abajstyles@gmail.com>
2025-08-12 09:23:51 +0530

    Create TelCoColorCoder_test.cpp

---
+++ TelCoColorCoder_test.cpp
@@ -0,0 +1,12 @@
+void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+    assert(colorPair.getMajor() == expectedMajor);
+    assert(colorPair.getMinor() == expectedMinor);
+}
+
+void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
+    int pairNumber = GetPairNumberFromColor(major, minor);
+    std::cout << "Got pair number " << pairNumber << std::endl;
+    assert(pairNumber == expectedPairNumber);
+}
